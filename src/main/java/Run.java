0d39a8848e4e0a5b3879886aeb1cd44548890cdf
Administrator <user@email.com>
2020-01-31 21:17:08 +0800

    视频转码压缩做不出来，先这样，手动压缩，再传

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -3,10 +3,7 @@ import util.FreemakerUtil;
 import util.QiniuUtil;
 import util.UuidUtil;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,23 +14,11 @@ import java.util.Map;
  */
 public class Run {
     public static void main(String[] args) throws Exception {
-//        String folder = "C:\\Users\\Administrator\\Videos\\Desktop\\";
-        String folder = "C:\\Users\\Administrator\\Videos\\Desktop";
-        String filename = "2020.01.31GoogleEarth演示.mp4";
+        String folder = "C:\\Users\\Administrator\\Videos\\Desktop\\";
+        String filename = "2020.01.31GoogleEarth演示_out.mp4";
         File videoFile = new File(folder, filename);
         String videoName = FilenameUtils.getBaseName(videoFile.getName());
         String uuid = UuidUtil.getUuid();
-        //视频转码
-        File newVideoFile = new File(folder, videoName + "_out.mp4");
-        Process process = Runtime.getRuntime().exec("ffmpeg.exe -i " + videoFile.getAbsolutePath()
-                + " -r 30 " + videoName + "_out.mp4");
-        InputStream fis = process.getInputStream();
-        InputStreamReader isr = new InputStreamReader(fis);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
         //上传视频
         String videoUrl = QiniuUtil.upload(videoFile.getAbsolutePath(),
                 videoName + "-" + uuid + "." + FilenameUtils.getExtension(videoFile.getName())
@@ -45,9 +30,8 @@ public class Run {
         params.put("videoUrl", videoUrl);
         File htmlFile = FreemakerUtil.createHtmlByMode("video.html.ftl", "video.html", params);
         //上传html
-        String htmlUrl = QiniuUtil.upload(htmlFile.getPath(),
-                videoName.replace(" ", "-")
-                        + "-" + uuid + "." + FilenameUtils.getExtension(htmlFile.getName()));
+        String htmlUrl = QiniuUtil.upload(htmlFile.getPath(), videoName.replace(" ", "-")
+                + "-" + uuid + "." + FilenameUtils.getExtension(htmlFile.getName()));
         //拿到链接
         System.out.println(htmlUrl);
         //删除html文件
